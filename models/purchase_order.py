b91ffcbbd1d84655cc567d824e1c2c2d61864cab
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-07 11:37:31 +0100

    [FIX] Fix useless Python imports.

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from datetime import datetime, time
-from dateutil.relativedelta import relativedelta
-from functools import partial
-from itertools import groupby
-import json
 
-from markupsafe import escape, Markup
-from pytz import timezone, UTC
-from werkzeug.urls import url_encode
-
-from odoo import api, fields, models, _
-from odoo.osv import expression
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from odoo.tools.float_utils import float_compare, float_is_zero, float_round
-from odoo.exceptions import AccessError, UserError, ValidationError
-from odoo.tools.misc import formatLang, get_lang, format_amount
+from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -29,7 +15,8 @@ class PurchaseOrder(models.Model):
         for each in self:
             if each.date_approve:date_approve_by_order.update({each.id:each.date_approve})
         res = super(PurchaseOrder, self).button_approve(force=force)
-        #FIXME:we have to discuss this solutions from performance point of view (send sql query as many as records in rcordset is bad,bu is the ORM take in care this?)
+        #FIXME:we have to discuss this solutions from performance point of view (send sql query as many as records in rcordset is bad,
+        # but is the ORM take in care this?),however this solution ensure the call the super method which is crucial
         for each in self:
             each.write({"date_approve":date_approve_by_order.get(each.id)})
         return res
